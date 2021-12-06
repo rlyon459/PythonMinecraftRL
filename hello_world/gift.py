12757d501ab0ed4b15c3ff348768b60ec5608749
Psalm Jones <pjones889@lsr7.net>
2021-12-06 14:13:07 -0600

    finally finished gift.py

--- hello_world/gift.py
+++ hello_world/gift.py
@@ -1,16 +1,15 @@
 from mcpi.minecraft import Minecraft
-mc = Minecraf.create()
-x = 10
-y = 11
-z = 12
+mc = Minecraft.create()
+x = 58.7
+y = 4.0
+z = -10.3
 gift = mc.getBlock(x, y, z,)
  #if gift is a diamond block
-if gift == 57
-    mc.setBlock(21, 21, 21,)
-
+if gift == 57:
+    mc.postToChat("thanks for the diamond")
 #else if gift is a sapling
-elif mc.setBlock(22, 22, 22,)
-
+elif gift == 6:
+    mc.postToChat("i guess tree saplings are as good as diamonds.....")
 else:
-    mc.postToChat("Bring a gift to" + str(x) + "," + str(y) + "," + str(z))
+    mc.postToChat("Bring a gift to " + str(x) + "," + str(y) + "," + str(z))
     
