506c33348b5564dc26905e0835575eef35e69fd7
Psalm Jones <pjones889@lsr7.net>
2022-01-03 14:15:18 -0600

    beggining of chapter 9

---
+++ hello_world/highAndLow.py
@@ -0,0 +1,19 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+height = [100, 0]
+count = 0
+while count < 20:
+    pos = mc.player.getTilePos()
+    
+    if pos.y < height[0]:
+        height[0] = pos.y
+    
+    elif pos.y > height[1]:
+        height[1] = pos.y
+        
+    count += 1
+    time.sleep(1)
+    
+mc.postToChat("lowest: " + str(height[0]))
+mc.postToChat("lowest: " + str(height[1]))
---
+++ hello_world/pogressBar.py
@@ -0,0 +1,8 @@
+pos = mc.player.getTilePos()
+x = pos.x + 1
+y = pos.y
+z = pos.z
+ #add 10 blocks (ID 20 ) empty list
+blocks.append(20, 20, 20, 20, 20, 20, 20, 20, 20, 20,)
+blocks = [ ]
+Block = 22
