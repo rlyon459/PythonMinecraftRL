1ef1073cefb3b0ccac4b1af12e627b1068cae557
Psalm Jones <pjones889@lsr7.net>
2022-02-01 14:11:17 -0600

    some of ch 10

---
+++ hello_world/pyramid.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+block = 24 # sandstone
+height = 10
+levels = range(height)
+
+pos = mc.player.getTilePos()
+x, y, z = pos.x + height, pos.y, pos.z
+
+for level in reversed(levels):
+    mc.setBlocks(x - level, y, z - level, x + level, y, z + level, block)
+    y += 1
