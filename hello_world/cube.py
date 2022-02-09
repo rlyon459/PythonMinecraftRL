eb12bc87d90356fbaf4fe42925f8802b414454f1
Psalm Jones <pjones889@lsr7.net>
2022-02-09 14:10:56 -0600

    cube.py

---
+++ hello_world/cube.py
@@ -0,0 +1,24 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+pos = mc.player.getTilePos()
+x = pos.x
+y = pos.y
+z = pos.z
+cube = [[[57, 57, 57, 57], [57, 0 , 0, 57], [57, 0, 0, 57], [57, 57, 57, 57]],
+        [[57, 0, 0, 57], [0, 0, 0, 0], [0, 0, 0, 0], [57, 0, 0, 57]],
+        [[57, 0, 0, 57], [0, 0, 0, 0], [0, 0, 0, 0], [57, 0, 0, 57]],
+        [[57, 57, 57, 57], [57, 0, 0, 57], [57, 0, 0, 57], [57, 57, 57, 57]]]
+
+startingX = x
+startingY = y
+
+for depth in cube:
+    for height in reversed(depth):
+        for block in height:
+            mc.setBlock(x + 1, y, z, block)
+            x += 1
+        y += 1
+        x = startingX
+    z += 1
+    y = startingY
