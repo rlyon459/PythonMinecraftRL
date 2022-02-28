54802a3b2baa25c842e0f6041eafc72cfd037a34
Psalm Jones <pjones889@lsr7.net>
2022-02-28 14:12:55 -0600

    some of chapt 11

--- hello_world/saveStructure.py
+++ hello_world/saveStructure.py
@@ -9,20 +9,20 @@ def sortPair(val1, val2):
     else:
         return val1, val2
 
-def copyStructure(x1, y1, x2 y2, z2):
+def copyStructure(x1, y1, z1, x2, y2, z2):
     x1, x2 = sortPair(x1, x2)
     y1, y2 = sortPair(y1, y2)
     z1, z2 = sortPair(z1, z2)
-        
-    width = x2 -x1
+    
+    width = x2 - x1
     height = y2 - y1
     length = z2 - z1
-        
+    
     structure = []
-        
+    
     print("Please wait...")
-        
-    #copy the structure
+    
+    #Copy the structure
     for row in range(height):
         structure.append([])
         for column in range(width):
@@ -30,8 +30,18 @@ def copyStructure(x1, y1, x2 y2, z2):
             for depth in range(length):
                 block = mc.getBlock(x1 + column, y1 + row, z1 + depth)
                 structure[row][column].append(block)
+    
     return structure
-#get the position of the first corner
+
+# Get the position of the first corner
+input("Move to the first position and press ENTER in this window")
+pos1 = mc.player.getTilePos()
+
+x1 = pos1.x
+y1 = pos1.y
+z1 = pos1.z
+
+# Get the position of the second corner
 input("Move to the first position and press ENTER in this window")
 pos2 = mc.player.getTilePos()
 
@@ -39,5 +49,8 @@ x2 = pos2.x
 y2 = pos2.y
 z2 = pos2.z
 
-structure = copyStructure( x1, y1, z1, x2, y2, x2)
-#store the structure in a file.
+structure = copyStructure(x1, y1, z1, x2, y2, z2)
+
+saveFile = open("saveStructure.txt", "a")
+saveFile.write(str(structure))
+saveFile.close()
